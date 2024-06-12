6d09cf3f363b7cf78888e83b7326841d747e90db
marcos-mmvv <marcosveras.mv@hotmail.com>
2024-06-12 09:07:36 -0300

    Repositorio  atualizado por automação v2.

--- main.py
+++ main.py
@@ -5,49 +5,34 @@ import os
 auto.PAUSE = 0.5
 #Abrir a tecla windows e chamar o powershell
 auto.press('win')
-auto.write('powershell')
+auto.write('vs code')
 auto.press('enter')
-time.sleep(2)
-#Entrar no diretorio do programa
-auto.write('cd python')
-auto.press('enter')
-time.sleep(2)
-auto.write('cd git_auto')
-auto.press('enter')
-auto.write('dir')
-auto.press('enter')
+time.sleep(7)
+#Abrir o terminal
+auto.hotkey('crtl', 'shift', "'")
+time.sleep(5)
 #Inicio do commit
-auto.write('pip freeze > requiriments.txt')
-auto.press('enter')
-time.sleep(2)
 auto.write('git init')
 auto.press('enter')
-time.sleep(2)
+time.sleep(3)
 auto.write('git add .')
-auto.press('enter')
-time.sleep(2)
-auto.write('git commit -m "Repositório atualizado por automação."')
 auto.press('enter')
 time.sleep(3)
 auto.write('git branch -M main')
 auto.press('enter')
-time.sleep(2)
+time.sleep(3)
+auto.write('git commit -m Rep atualizado por automação v2.')
+auto.press('enter')
+time.sleep(3)
 auto.write('git remote add origin https://github.com/Marcos-mmvv/git_auto.git')
 auto.press('enter')
 time.sleep(2)
-auto.write('git push -u origin main')
+auto.write('git push')
 auto.press('enter')
-time.sleep(3)
-auto.write('git push -u origin main')
-auto.press('enter')
-time.sleep(3)
-#auto.write('cxfreeze main.py -- target-dir pasta-gitauto')
-#auto.press('enter')
-#Inicio do Build
-auto.write('cxfreeze main.py -- target-dir pasta-gitauto')
-time.sleep(8)
-auto.press('enter')
-#auto.hotkey('alt', 'f4')
+
+
+
+
 
 
 
